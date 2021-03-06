619363948df039b60ea0b2f4a5f54e52dbddfe1a
ZeroByter <romgamesrom@gmail.com>
2021-03-06 23:14:00 +0200

    started working on super basic client/server entities sync, might switch to using ts soon

--- src/client/pages/App.js
+++ src/client/pages/App.js
@@ -1,11 +1,35 @@
 import React, { createRef } from 'react';
+import { defaultMap, snoodSize } from '../../shared/constants';
 import './app.scss';
 
 export default class App extends React.Component {
+	snoods = []
+	snoodsMap = {}
+	viewOffset = {
+		x: 0,
+		y: 0
+	}
+
 	canvasRef = createRef()
 	canvasRenderInterval = -1
 
 	componentDidMount() {
+		this.props.socket.on("newEntity", data => {
+			if (data.type === "snood") {
+				this.snoodsMap[data.id] = this.snoods.length
+				this.snoods.push(data.location)
+			}
+		})
+		this.props.socket.on("allEntities", data => {
+			Object.entries(data.snoods).forEach(entry => {
+				const id = entry[0]
+				const snood = entry[1]
+
+				this.snoodsMap[id] = this.snoods.length
+				this.snoods.push(snood)
+			})
+		})
+
 		const canvas = this.canvasRef.current
 
 		const width = canvas.width
@@ -13,19 +37,32 @@ export default class App extends React.Component {
 
 		const ctx = canvas.getContext("2d")
 
+		const scale = 12
+
 		this.canvasRenderInterval = setInterval(() => {
 			ctx.fillStyle = "white"
 			ctx.fillRect(0, 0, width, height)
 
-			ctx.fillStyle = "black"
-			ctx.beginPath()
-			ctx.moveTo(0, 0)
-			ctx.lineTo(100, 50)
-			ctx.lineTo(100, 25)
-			ctx.closePath()
-			ctx.strokeStyle = "#000000";
-			ctx.lineWidth = 1;
-			ctx.stroke()
+			ctx.strokeStyle = "black";
+			ctx.lineWidth = 1
+			defaultMap.borders.forEach(border => {
+				ctx.beginPath()
+				border.forEach(point => {
+					ctx.lineTo(point[0] * scale + this.viewOffset.x, point[1] * scale + this.viewOffset.y)
+				})
+				ctx.closePath()
+				ctx.stroke()
+			})
+
+			ctx.lineWidth = snoodSize * scale * 0.3;
+			ctx.fillStyle = "green"
+			ctx.strokeStyle = '#003300';
+			this.snoods.forEach(snood => {
+				ctx.beginPath();
+				ctx.arc(snood.x * scale + this.viewOffset.x, snood.y * scale + this.viewOffset.y, snoodSize * scale, 0, 2 * Math.PI, false);
+				ctx.fill();
+				ctx.stroke();
+			})
 		}, 50)
 	}
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { defaultMap, snoodSize } = require('../shared/constants');
+const { randomString } = require('../shared/essentials');
 const app = express();
 
 const http = require("http").Server(app)
@@ -11,8 +13,51 @@ app.use(express.static('dist'));
 //api example
 //app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
+const currentMap = defaultMap
+let snoods = {}
+
+snoods[randomString()] = {
+    x: defaultMap.initialsnoodPosition[0],
+    y: defaultMap.initialsnoodPosition[1],
+    fertile: false,
+}
+
+const newsnood = (x, y) => {
+    let newsnoodId = randomString()
+    let newsnood = {
+        x,
+        y
+    }
+
+    snoods[newsnoodId] = newsnood
+
+    io.emit("newEntity", {
+        type: "snood",
+        id: newsnoodId,
+        location: {
+            x: newsnood.x,
+            y: newsnood.y,
+        }
+    })
+
+    return newsnoodId
+}
+
+setInterval(() => {
+    Object.entries(snoods).forEach(entry => {
+        const id = entry[0]
+        const snood = entry[1]
+
+        //newsnood(snood.x + snoodSize, snood.y)
+    })
+}, 2000)
+
 io.on("connection", socket => {
     socket.emit("getId", socket.id)
+
+    socket.emit("allEntities", {
+        snoods
+    })
 })
 
 http.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
